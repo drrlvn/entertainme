2067e963c38b5e2f465dc7bccf09e5afc7eb3df9
Dror Levin <dror@psybear.com>
2024-09-23 12:21:40 +0200

    Remove howlongtobeat since it needs to be rewritten

--- src/games.rs
+++ src/games.rs
@@ -1,8 +1,7 @@
 use super::{Error, Result};
-use futures::future::{join3, join_all, try_join_all};
+use futures::future::{join, join_all, try_join_all};
 use std::fmt::Display;
 
-mod howlongtobeat_api;
 mod opencritic_api;
 mod steam_api;
 
@@ -10,16 +9,14 @@ mod steam_api;
 pub struct GameData {
     pub steam: Option<steam_api::GameData>,
     pub opencritic: Option<opencritic_api::GameData>,
-    pub howlongtobeat: Option<howlongtobeat_api::GameData>,
 }
 
 impl GameData {
     async fn get(names: &super::Names) -> Result<Self> {
         let results = join_all(names.0.iter().map(|name| {
-            join3(
+            join(
                 steam_api::GameData::get(name.clone()),
                 opencritic_api::GameData::get(name.clone()),
-                howlongtobeat_api::GameData::get(name.clone()),
             )
         }))
         .await;
@@ -27,26 +24,20 @@ impl GameData {
         let mut game_data = GameData {
             steam: None,
             opencritic: None,
-            howlongtobeat: None,
         };
 
-        for (steam, opencritic, howlongtobeat) in results {
-            let (steam, opencritic, howlongtobeat) = (
-                parse_result(steam)?,
-                parse_result(opencritic)?,
-                parse_result(howlongtobeat)?,
-            );
+        for (steam, opencritic) in results {
+            let (steam, opencritic) = (parse_result(steam)?, parse_result(opencritic)?);
 
             game_data.steam = game_data.steam.or(steam);
             game_data.opencritic = game_data.opencritic.or(opencritic);
-            game_data.howlongtobeat = game_data.howlongtobeat.or(howlongtobeat);
 
-            if game_data.steam.is_some() && game_data.opencritic.is_some() && game_data.howlongtobeat.is_some() {
+            if game_data.steam.is_some() && game_data.opencritic.is_some() {
                 break;
             }
         }
 
-        if game_data.steam.is_some() || game_data.opencritic.is_some() || game_data.howlongtobeat.is_some() {
+        if game_data.steam.is_some() || game_data.opencritic.is_some() {
             Ok(game_data)
         } else {
             Err(Error::NotFound)
@@ -58,8 +49,6 @@ impl GameData {
             &steam.name
         } else if let Some(opencritic) = &self.opencritic {
             &opencritic.name
-        } else if let Some(howlongtobeat) = &self.howlongtobeat {
-            &howlongtobeat.name
         } else {
             ""
         }
@@ -70,8 +59,7 @@ impl Display for GameData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "{}", self.name())?;
         format_option(f, "Steam", &self.steam)?;
-        format_option(f, "Opencritic", &self.opencritic)?;
-        format_option(f, "How Long To Beat", &self.howlongtobeat)
+        format_option(f, "Opencritic", &self.opencritic)
     }
 }
 
